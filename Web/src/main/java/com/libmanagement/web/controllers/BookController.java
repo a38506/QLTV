4e90cc041554022c473f3db0d3e6b68abcb16b69
TuanKietTran <kyletran101.work@gmail.com>
2023-11-09 09:33:05 +0700

    Sample Controller

--- Web/src/main/java/com/libmanagement/web/controllers/BookController.java
+++ Web/src/main/java/com/libmanagement/web/controllers/BookController.java
@@ -1,11 +1,30 @@
 package com.libmanagement.web.controllers;
 
+import com.libmanagement.core.models.Book;
+import com.libmanagement.core.services.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/api/book")
 public class BookController {
+    // Spring Boot
+    private final BookService _bookService;
+    BookController(BookService bookService) {
+        _bookService = bookService;
+    }
 
+    @GetMapping()
+    public List<Book> getAllBooks() {
+        return _bookService.getAllBooks();
+    }
 
+    @PostMapping()
+    public void addNewBook(Book book) {
+        _bookService.addBook(book.getName_Book(), book.get_Author(), book.get_Major());
+    }
 }
---
+++ Web/src/main/java/com/libmanagement/web/controllers/StudentController.java
@@ -0,0 +1,10 @@
+package com.libmanagement.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student") // decorator
+public class StudentController {
+
+}
